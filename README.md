626259765bd3d01262782493250ade8cef25ae0c
Cobra <c8d8z8@gmail.com>
2019-11-04 11:16:44 +0800

    登录

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cobra
 后台管理系统
+
+## 背景
+知识梳理，将分散的知识串起来
+
+## 工具
++ IntelliJ Idea CE
++ MySQLWorkbench
+
+## 技术栈
++ Spring Boot
++ Spring Security
++ Mybatis & MybatisGenerator
++ Bootstrap Table
++ Shiro
+
+## 功能列表
++ 数据库字段管理
++ 代码自动生成 
+    + 定义BaseEntity BaseMapper BaseService BaseController
---
+++ cobra-spring-boot/src/main/java/org/github/omnbmh/cobra/CobraApplication.java
@@ -0,0 +1,11 @@
+package org.github.omnbmh.cobra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CobraApplication.class, args);
+    }
+}
